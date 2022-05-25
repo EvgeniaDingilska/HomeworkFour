9e98736c1ed5893fc72d9db9770441190921db5b
EvgeniaDingilska <dingilska@gmail.com>
2022-05-26 00:15:23 +0300

    dddd

--- src/Exercise/BeachOrMountain.java
+++ src/Exercise/BeachOrMountain.java
@@ -1,4 +1,47 @@
 package Exercise;
 
+import java.util.Scanner;
+
 public class BeachOrMountain {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String destination;
+        double price;
+
+           System.out.println("Please write a 'b' for beach and 'm' for mountain:");
+           destination = scanner.nextLine();
+
+           System.out.println("Please write a price:");
+           price = scanner.nextDouble();
+
+        if (destination.equals("m") || destination.equals("b") || price > 0)
+        {
+            switch (destination) {
+                case "b":
+                    if (price <= 50)
+                    {
+                        System.out.println("We recommended Bulgaria");
+                    } else
+                    {
+                        System.out.println("We recommended outside of Bulgaria");
+                    }
+                    break;
+
+                case "m":
+                    if (price <= 30)
+                    {
+                        System.out.println("We recommended Bulgarian mountain");
+                    } else
+                    {
+                        System.out.println("We recommended outside of Bulgaria");
+                    }
+                    break;
+            }
+        }
+       else
+       {
+            System.out.println("There is no information about this type of vacation");
+        }
+    }
 }
--- src/Exercise/DivideByFive.java
+++ src/Exercise/DivideByFive.java
@@ -1,2 +1,19 @@
-package Exercise;public class DivideByFive {
+package Exercise;
+
+public class DivideByFive {
+    public static void main(String[] args) {
+        int [] arr = {12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200};
+
+
+        for (int i = 0 ; i < arr.length; i++)
+        {
+           if (arr[i]>150)
+           {
+               break;
+           } else if (arr[i]%5 == 0)
+           {
+               System.out.print(arr[i] + ", ");
+           }
+        }
+    }
 }
